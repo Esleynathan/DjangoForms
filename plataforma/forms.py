410b78cc5aeb31a6d1696d95bfef5faf205a1b9e
Esley <esleynathan@hotmail.com>
2023-02-21 15:55:11 -0300

    DjangoForms | Adicinando Atributos

--- plataforma/forms.py
+++ plataforma/forms.py
@@ -1,5 +1,14 @@
 from django import forms
 
 class Cliente(forms.Form):
-    nome = forms.CharField(max_length=100)
-    idade = forms.IntegerField()
+    nome = forms.CharField(max_length=100, required=False)
+    idade = forms.IntegerField()    
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['nome'].widget.attrs['class'] = 'form-control'
+        self.fields['nome'].widget.attrs['placeholder'] = 'Digite seu nome...'   
+        
+        self.fields['idade'].widget.attrs['class'] = 'form-control'     
+        self.fields['idade'].widget.attrs['placeholder'] = 'Digite sua idade...'
+            
--- plataforma/views.py
+++ plataforma/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-from forms import Cliente
+from .forms import Cliente
 
 @login_required(login_url= '/auth/login')
 def home(request):
         form = Cliente()
         return render(request, 'home.html', {'form': form})
+        
