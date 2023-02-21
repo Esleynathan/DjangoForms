1bdcaedc9f38745653044c402c9533e59d9775bc
Esley <esleynathan@hotmail.com>
2023-02-21 15:23:41 -0300

    Utilizando formulários como P, UL e TABLE

--- plataforma/views.py
+++ plataforma/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from forms import Cliente
 
 @login_required(login_url= '/auth/login')
 def home(request):
-        return render(request, 'home.html')
+        form = Cliente.form
+        return render(request, 'home.html', {'form': form})
