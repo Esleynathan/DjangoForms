5927901f468400f14e58006f85fb1daaeccd68fc
Esley <esleynathan@hotmail.com>
2023-02-21 18:50:04 -0300

    Django Forms | Enviando e validando formulário

--- plataforma/views.py
+++ plataforma/views.py
@@ -6,17 +6,32 @@ from .forms import Cliente
 
 @login_required(login_url= '/auth/login')
 def home(request):
-	form = Cliente()
-
-	input_alterar= ('nome','idade')
-	for i in form.fields.keys():
-		for i in input_alterar:
-			try:
-				classe_anterior = form.fields[i].widget.attrs['class']
-			except KeyError:
-				classe_anterior = ""
-				
-			form.fields[i].widget.attrs['class'] += classe_anterior + " teste"
-
-	return render(request, 'home.html', {'form': form})
+	if request.method == "GET":
+		form = Cliente()
+		return render(request, 'home.html', {'form': form})
+	elif request.method == "POST":
 		
+		form = Cliente(request.POST)	
+
+		if form.is_valid():
+			nome = form.data['nome']
+			idade = form.data['idade']		
+			data = form.data['data']			
+			email = form.data['email']
+			form.cleaned_data
+			return HttpResponse('Formulário enviado')
+		else:
+			return render(request, 'home.html', {'form': form})
+
+		
+	
+
+	# input_alterar= ('nome','idade')
+	# for i in form.fields.keys():
+	# 	for i in input_alterar:
+	# 		try:
+	# 			classe_anterior = form.fields[i].widget.attrs['class']
+	# 		except KeyError:
+	# 			classe_anterior = ""
+
+	# 		form.fields[i].widget.attrs['class'] += classe_anterior + " teste"
